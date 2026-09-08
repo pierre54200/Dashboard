12aa54e69e47241bbc2564221cb36f72df0782d0
ILPinciatore <pierre.maciejewski@epitech.eu>
2026-09-08 17:36:47 +0200

    Add /login routes

--- frontend/Dashboard/src/app/app.ts
+++ frontend/Dashboard/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('frontend');
+  protected readonly title = signal('Dashboard');
 }
