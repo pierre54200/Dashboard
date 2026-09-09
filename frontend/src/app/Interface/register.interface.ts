7a4ca18afc36e57461b87830abf4e46bfede240d
ILPinciatore <pierre.maciejewski@epitech.eu>
2026-09-09 16:41:35 +0200

    Add Register Interface

---
+++ frontend/src/app/Interface/register.interface.ts
@@ -0,0 +1,9 @@
+export interface RegisterInterface {
+    username: string,
+    email: string,
+    password: string,
+    birthdate: Date,
+    address: string,
+    pays: string,
+    ville: string,
+}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './Component/login/login';
+import { LoginComponent } from './Component/login/login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
