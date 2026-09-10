98cdd7a9910f68fd7a24b465e24dea2d1428686f
ILPinciatore <pierre.maciejewski@epitech.eu>
2026-09-10 14:45:58 +0200

    Add Drage and drop

--- frontend/src/app/Component/Auth/login/login.component.ts
+++ frontend/src/app/Component/Auth/login/login.component.ts
@@ -1,9 +1,15 @@
 import { Component } from "@angular/core";
+import {CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
-  imports: [],
+  imports: [CdkDropList, CdkDrag],
   selector: "app-login",
   styleUrl: "./login.component.css",
   templateUrl: "./login.component.html",
 })
-export class LoginComponent {}
+export class LoginComponent {
+    items = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+      drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+      }
+}
