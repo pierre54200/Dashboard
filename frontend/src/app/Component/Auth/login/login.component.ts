6a59af26daac084bb3fa34223d57433931351924
ILPinciatore <pierre.maciejewski@epitech.eu>
2026-09-14 14:05:01 +0200

    Replace SCSS by Tailwind CSS

--- frontend/src/app/Component/Auth/login/login.component.ts
+++ frontend/src/app/Component/Auth/login/login.component.ts
@@ -1,15 +1,9 @@
-import { Component } from "@angular/core";
-import {CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray} from '@angular/cdk/drag-drop';
+import { Component } from '@angular/core';
 
 @Component({
-  imports: [CdkDropList, CdkDrag],
-  selector: "app-login",
-  styleUrl: "./login.component.css",
-  templateUrl: "./login.component.html",
+  imports: [],
+  selector: 'app-login',
+  styleUrl: './login.component.css',
+  templateUrl: './login.component.html',
 })
-export class LoginComponent {
-    items = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
-      drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
-      }
-}
+export class LoginComponent {}
