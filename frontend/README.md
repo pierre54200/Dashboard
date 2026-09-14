329814fcb771d613748797a2cdf29f2b5f0b9ef5
ILPinciatore <pierre.maciejewski@epitech.eu>
2026-09-14 14:05:01 +0200

    Replace SCSS by Tailwind CSS

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# Dashboard
+# Frontend
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 22.1.7.
 
--- frontend/src/app/Component/Auth/login/login.component.spec.ts
+++ frontend/src/app/Component/Auth/login/login.component.spec.ts
@@ -1,7 +1,7 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { LoginComponent } from "./login.component";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LoginComponent } from './login.component';
 
-describe("LoginComponent", () => {
+describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
@@ -15,7 +15,7 @@ describe("LoginComponent", () => {
     await fixture.whenStable();
   });
 
-  it("should create", () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
