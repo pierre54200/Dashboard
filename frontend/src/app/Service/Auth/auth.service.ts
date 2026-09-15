132025eb8039b750826a65df47d2b220d9c95bee
ILPinciatore <pierre.maciejewski@epitech.eu>
2026-09-15 16:19:59 +0200

    Add Auth Service

---
+++ frontend/src/app/Service/Auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../../Environments/environment";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+    private apiUrl = `${environment.apiUrl}/test`
+
+    constructor(private http: HttpClient) {}
+
+    getTest() {
+        return this.http.get(this.apiUrl)
+    }
+}
