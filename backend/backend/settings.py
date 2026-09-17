ff3df382fdca43cd2e84c6418ba8268ebc75c26e
ILPinciatore <pierre.maciejewski@epitech.eu>
2026-09-17 14:50:07 +0200

    Update gitignore

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tnexd)#8^rjy_c8c7ip62phb15%6+4i%b!ka9tf7_lcdmuixd2'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ frontend/src/app/Component/Settings/profil/profil.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  imports: [],
+  selector: "app-profil",
+  styleUrl: "./profil.component.css",
+  templateUrl: "./profil.component.html",
+})
+export class ProfilComponent {}
